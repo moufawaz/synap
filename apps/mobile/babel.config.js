2c7bbf4ddc778f84f275c9f4748c43b905cf7989
SYNAP Dev <dev@synapfit.app>
2026-05-22 20:57:28 +0300

    Fix iOS JSC bundle transform

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['@babel/plugin-transform-classes', { loose: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
